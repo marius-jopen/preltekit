286322577b27b5784ef1e0987fefdb1d6269ac95
Marius Jopen <marius.jopen@thepeople.world>
2023-05-18 21:16:49 +0400

    index

--- src/lib/modules/index/index-items.svelte
+++ src/lib/modules/index/index-items.svelte
@@ -4,7 +4,7 @@
 	export let items
 </script>
 
-<div class="sm:flex flex-wrap justify-center">
+<div class="w-full px-10 sm:flex flex-wrap justify-center">
 	{#each items as item (item.item.uid)}
 		<ItemProject item={item.item} />
 	{/each}
